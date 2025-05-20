897162eba96bcf50f22248ea0cd01aa7fcb034f4
Pjmaciel <pjmaciel_4@hotmail.com>
2025-05-20 17:44:45 -0300

    build: initial db config

---
+++ src/libary_j2/DbException.java
@@ -0,0 +1,12 @@
+package libary_j2;
+
+
+public class DbException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbException(String msg) {
+		super(msg);
+	}
+
+}
